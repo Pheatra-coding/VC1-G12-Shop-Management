6b6795615c7210a72e80de6f59e53ec9b45e12b5
darinhoy <darin.hoy@student.passerellesnumeriques.org>
2025-03-08 00:23:51 +0700

    feat(delete product) : added delete products

--- Router/route.php
+++ Router/route.php
@@ -17,11 +17,14 @@ $route->get("/products/create", [ProductController::class, 'create']);
 $route->post("/products/store", [ProductController::class, 'store']);
 $route->get("/products/edit/{id}", [ProductController::class, 'edit']);
 $route->put("/products/update/{id}", [ProductController::class, 'update']);
+$route->delete("/products/delete/{id}", [ProductController::class, 'delete']);
+
 
 $route->route();
 //users
 $route->get("/users", [UserController::class, 'index']);
 $route->get("/users/create", [UserController::class, 'create']);
 $route->get("/users/store", [UserController::class, 'store']);
+
 
 $route->route();
