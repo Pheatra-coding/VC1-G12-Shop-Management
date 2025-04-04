3904a76eee1ecaf72919a733e8866dd54cfca341
Pheatra-coding <oempheaktra877@gmail.com>
2025-04-04 11:12:07 +0700

    feat(telegram-alert) add Top Selling alert to telegram [VG-368]

--- Controllers/TopSellingController.php
+++ Controllers/TopSellingController.php
@@ -1,15 +1,45 @@
 <?php
 require_once 'Models/TopSellingModel.php';
 
-class TopSellingController extends BaseController {
-    // Get top-selling products
-    public function index() {
+class TopSellingController extends BaseController 
+{
+    private $lastAlertTime = 0;
+    private $alertInterval = 600; // 10 minutes
+
+    public function index() 
+    {
         $model = new TopSellingModel();
         $topSellingProducts = $model->getTopSellingProducts();
         
-        // Pass the data to the view
+        // Auto-send alerts
+        $this->autoSendAlerts($topSellingProducts);
+        
         $this->view('sales/top_selling', [
-            'products' => $topSellingProducts,
+            'products' => $topSellingProducts
         ]);
+    }
+
+    private function autoSendAlerts($products) 
+    {
+        $currentTime = time();
+        if ($currentTime - $this->lastAlertTime >= $this->alertInterval && !empty($products)) {
+            $this->sendTopSellingAlert($products);
+            $this->lastAlertTime = $currentTime;
+        }
+    }
+
+    private function sendTopSellingAlert($products) 
+    {
+        $message = $this->formatTelegramAlert(
+            $products,
+            '🚀 Top Selling Products Alert!',
+            function($product) {
+                $name = $product['name'] ?? 'Unknown Product';
+                $sold = $product['sold'] ?? $product['total_sold'] ?? $product['quantity_sold'] ?? 'N/A';
+                return "• {$name} - Sold: {$sold}";
+            }
+        );
+        
+        $this->sendTelegramMessage($message);
     }
 }
